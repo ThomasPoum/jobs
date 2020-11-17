dedb4e9bf3616db9eed7168cd5f160c079278bf0
Thomas POUMAREDE <thomaspoumarede@MacBook-Pro-de-thomas-2.home>
2020-11-17 18:05:03 +0100

    change database to postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_11_17_145629) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "action_text_rich_texts", force: :cascade do |t|
     t.string "name", null: false
     t.text "body"
